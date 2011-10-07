1dc9be6895c64fa6fa01353811e3f9467f9942fd
SixArm <sixarm@sixarm.com>
2011-10-06 21:51:22 -0700

    Updates for gem publishing

--- lib/sixarm_ruby_active_record_uuid.rb
+++ lib/sixarm_ruby_active_record_uuid.rb
@@ -1,27 +1,6 @@
 # -*- coding: utf-8 -*-
-
 =begin rdoc
-
-= SixArm.com » Ruby » Active Record UUID module to use UUID for ActiveRecord id
-
-Author:: Joel Parker Henderson, joelparkerhenderson@gmail.com
-Copyright:: Copyright (c) 2006-2011 Joel Parker Henderson
-License:: See LICENSE.txt file
-
-ActiveRecordUUID enables Active Record models to use a UUID for the id field.
-
-This creates a UUID in the Active Record model's before_save method.
-
-Based on "Using UUID/GUID as Primary Key in Rails" by Paul Dix:
-http://codesnipers.com/?q=node/143&title=using-uuid/guid-as-primary-key-in-rails
-
-
-@example
-
-  class Item < ActiveRecord::Base
-    include ActiveRecordUUID
-  end
-
+Please see README.rdoc
 =end
 
 module ActiveRecordUUID
--- test/sixarm_ruby_active_record_uuid_test.rb
+++ test/sixarm_ruby_active_record_uuid_test.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
+require 'test/unit'
 require 'simplecov'
 SimpleCov.start
-require 'test/unit'
 require 'sixarm_ruby_active_record_uuid'
 
 class ActiveRecordUUIDTest < Test::Unit::TestCase
